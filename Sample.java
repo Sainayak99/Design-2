b503fae60c339a84c47068ac7b893f342af77a5b
sainath k <sainathkodiripaka99@gmail.com>
2023-05-23 19:54:28 -0500

    Done

--- Sample.java
+++ Sample.java
@@ -1,7 +1,33 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(1)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
 
 
 // Your code here along with comments explaining your approach
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyQueue {
+    Queue<Integer> queue;
+    public MyQueue() {
+        queue = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue.add(x);
+    }
+    
+    public int pop() {
+       return queue.remove();
+    }
+    
+    public int peek() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
